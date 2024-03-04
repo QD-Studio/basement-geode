331eb4bce91035ebb31c0c40e79184b5c9eb6cf8
JaanDev <67502867+JaanDev@users.noreply.github.com>
2024-03-04 22:29:45 +0300

    russian input

--- src/RussianInput.cpp
+++ src/RussianInput.cpp
@@ -8,22 +8,22 @@ using namespace geode::prelude;
 
 class $modify(CCIMEDispatcher){
     void dispatchInsertText(const char* text, int len, cocos2d::enumKeyCodes idk) {
-        log::info("DIT {} {} {}", text, len, (int)idk);
+        // log::info("DIT {} {} {}", text, len, (int)idk);
         
-        for (auto p = text; *p != 0x0; p++) {
-            log::debug("0x{:X}", (int)*p);
-        }
+        // for (auto p = text; *p != 0x0; p++) {
+        //     log::debug("0x{:X}", (int)*p);
+        // }
         
         auto p1 = basementutils::cp1251_to_utf8(text);
 
-        log::debug("p1: {}", p1);
+        // log::debug("p1: {}", p1);
 
         if (strlen(p1.c_str()) == 1)
             return CCIMEDispatcher::dispatchInsertText(p1.c_str(), 1, idk);
 
         int cp = p1[1] << 8 | p1[0];
 
-        log::debug("cp 0x{:X}", cp);
+        // log::debug("cp 0x{:X}", cp);
         
         if (cp == 0x401 || (0x410 <= cp && cp <= 0x44F) || cp == 0x451) {
             char chars[5] = { '\0' };
@@ -32,22 +32,22 @@ class $modify(CCIMEDispatcher){
             
             auto a = std::string(chars);
 
-            log::debug("a: {}", a);
+            // log::debug("a: {}", a);
 
             return CCIMEDispatcher::dispatchInsertText(a.c_str(), 2, idk);
         } else {
-            log::debug("lol");
+            // log::debug("lol");
             return CCIMEDispatcher::dispatchInsertText(p1.c_str(), utf8::internal::sequence_length(p1.begin()), idk);
         }
 
-        log::debug("omega");
+        // log::debug("omega");
 
         CCIMEDispatcher::dispatchInsertText(p1.c_str(), len, idk);
     }
 };
 
 class $modify(CCTextInputNode) {
-    bool onTextFieldInsertText(cocos2d::CCTextFieldTTF*, char const*, int){
+    virtual bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* pSender, char const* text, int nLen, cocos2d::enumKeyCodes keyCodes) {
         return false;
     }
 };
